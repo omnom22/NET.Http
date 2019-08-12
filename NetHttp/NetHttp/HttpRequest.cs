cc025a7e6e38fe13bbe0451ccc9f9f02d044a75d
OnurOzkan22 <onur-ozkan@hotmail.de>
2019-08-12 20:24:23 +0200

    first scaffold

---
+++ NetHttp/NetHttp/HttpRequest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace NetHttp
+{
+    public enum HttpMethodType
+    {
+        POST,
+        GET,
+        DELETE,
+        PUT
+    }
+
+    public enum MediaType
+    {
+        JSON,
+        XML
+    }
+
+    public class HttpRequest
+    {
+
+        private string mediaType;
+
+        public HttpRequest(MediaType mediaType = MediaType.JSON)
+        {
+            switch (mediaType)
+            {
+                case MediaType.JSON:
+                    this.mediaType = "application/json";
+                    break;
+                case MediaType.XML:
+                    this.mediaType = "application/xml";
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Http get request
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns>Promise</returns>
+        public Promise Get(string url)
+        {
+            Promise p = new Promise(mediaType,url, HttpMethodType.GET);
+            return p;
+        }
+
+        /// <summary>
+        /// Http post request
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public Promise Post(string url,string data)
+        {
+            Promise p = new Promise(mediaType, url,data, HttpMethodType.POST);
+            return p;
+        }
+
+        public Promise Delete(string url)
+        {
+            Promise p = new Promise(mediaType, url, HttpMethodType.DELETE);
+            return p;
+        }
+
+        public Promise Put(string url, string data)
+        {
+            Promise p = new Promise(mediaType, url, data, HttpMethodType.PUT);
+            return p;
+        }
+
+    }
+}
---
+++ NetHttp/NetHttp/Promise.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace NetHttp
+{
+
+    public class Promise
+    {
+        private string url;
+        private string data;
+        private HttpMethodType method;
+        private string mediaType;
+
+        public Promise(string mediaType,string url,HttpMethodType method)
+        {
+            this.url = url;
+            this.method = method;
+            this.mediaType = mediaType;
+        }
+
+        public Promise(string mediaType,string url, string data, HttpMethodType method)
+        {
+            this.url = url;
+            this.data = data;
+            this.method = method;
+            this.mediaType = mediaType;
+        }
+
+        public async void Then(Action<string> func)
+        {
+            HttpClient client = new HttpClient();
+
+            using (HttpResponseMessage res = await GetHttpMethod(url,method))
+            {
+                using (HttpContent content = res.Content)
+                {
+                    string data = await content.ReadAsStringAsync();
+                    func(data);
+                }
+            }
+        }
+
+        async Task<HttpResponseMessage> GetHttpMethod(string url, HttpMethodType method)
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders
+                .Accept
+                .Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue(mediaType));
+
+            switch (method)
+            {
+                case HttpMethodType.POST:
+                    return await client.PostAsync(url,new StringContent(data));
+                case HttpMethodType.GET:
+                    return await client.GetAsync(url);
+                case HttpMethodType.DELETE:
+                    return await client.DeleteAsync(url);
+                case HttpMethodType.PUT:
+                    return await client.PutAsync(url, new StringContent(data));
+                default:
+                    throw new System.Exception("This http method does't exist or is currently not supported.");
+            }
+            
+        }
+
+    }
+}
